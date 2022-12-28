1f46359fc9155e0656e2b448885174ec0343e779
Qpx Design <71609644+QpxDesign@users.noreply.github.com>
2022-12-28 16:33:28 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-var mtaAPI = require("./routes/mtaAPI.cjs");
-var tripUpdatesAPI = require("./routes/tripUpdatesAPI.cjs");
+var mtaAPI = require("./routes/mtaAPI.mjs");
+var tripUpdatesAPI = require("./routes/tripUpdatesAPI.mjs");
 var cors = require("cors");
 
 var indexRouter = require("./routes/index");
